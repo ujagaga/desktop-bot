19efb890a277fdd9d3f822f8a32b30f2cbe748c7
Rada Berar <ujagaga@gmail.com>
2026-09-20 10:58:02 +0200

    Update

--- MiniBot/ESP32_S3/config.h
+++ MiniBot/ESP32_S3/config.h
@@ -2,7 +2,7 @@
 #define MINIBOT_CONFIG_H
 
 // Update before building a new firmware release.
-#define FIRMWARE_VERSION 6
+#define FIRMWARE_VERSION 7
 
 #define OTA_GITHUB_REPOSITORY "ujagaga/desktop-bot"
 #define OTA_GITHUB_BRANCH "main"
